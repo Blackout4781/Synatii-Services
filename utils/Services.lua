4dc5cbbf08230dc670e202c5d0572e93374cd3d5
blackout <74207478+Blackout4781@users.noreply.github.com>
2024-02-25 00:23:35 -0600

    Update Services.lua

--- utils/Services.lua
+++ utils/Services.lua
@@ -1,30 +1,30 @@
-local Services = {};
-local vim = getvirtualinputmanager and getvirtualinputmanager();
+local Services = {}
+local vim = getvirtualinputmanager and getvirtualinputmanager()
 
 function Services:Get(...)
-    local allServices = {};
+    local allServices = {}
 
-    for _, service in next, {...} do
-        table.insert(allServices, self[service]);
+    for _, service in ipairs{...} do
+        table.insert(allServices, self[service])
     end
 
-    return unpack(allServices);
-end;
+    return unpack(allServices)
+end
 
 setmetatable(Services, {
     __index = function(self, p)
-        if (p == 'VirtualInputManager' and vim) then
-            return vim;
-        end;
+        if p == 'VirtualInputManager' and vim then
+            return vim
+        end
 
-        local service = game:GetService(p);
-        if (p == 'VirtualInputManager') then
-            service.Name = getServerConstant('VirtualInputManager ');
-        end;
+        local service = game:GetService(p)
+        if p == 'VirtualInputManager' then
+            service.Name = getServerConstant('VirtualInputManager')
+        end
 
-        rawset(self, p, service);
-        return rawget(self, p);
+        rawset(self, p, service)
+        return rawget(self, p)
     end,
-});
+})
 
-return Services;
+return Services
