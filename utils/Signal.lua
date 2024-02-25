38481c64c1009437671ed7fcfec208645e8d9bfe
blackout <74207478+Blackout4781@users.noreply.github.com>
2024-02-25 00:10:51 -0600

    Update Signal.lua

--- utils/Signal.lua
+++ utils/Signal.lua
@@ -13,64 +13,69 @@ Signal.ClassName = "Signal"
 -- @constructor Signal.new()
 -- @treturn Signal
 function Signal.new()
-	local self = setmetatable({}, Signal)
+    local self = setmetatable({}, Signal)
 
-	self._bindableEvent = Instance.new("BindableEvent")
-	self._argData = nil
-	self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
+    self._bindableEvent = Instance.new("BindableEvent")
+    self._argData = nil
+    self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
 
-	return self
+    return self
 end
 
 function Signal.isSignal(object)
-	return typeof(object) == 'table' and getmetatable(object) == Signal;
-end;
+    return typeof(object) == 'table' and getmetatable(object) == Signal
+end
 
 --- Fire the event with the given arguments. All handlers will be invoked. Handlers follow
 -- Roblox signal conventions.
 -- @param ... Variable arguments to pass to handler
 -- @treturn nil
 function Signal:Fire(...)
-	self._argData = {...}
-	self._argCount = select("#", ...)
-	self._bindableEvent:Fire()
-	self._argData = nil
-	self._argCount = nil
+    self._argData = {...}
+    self._argCount = select("#", ...)
+    self._bindableEvent:Fire()
+    self._argData = nil
+    self._argCount = nil
 end
 
 --- Connect a new handler to the event. Returns a connection object that can be disconnected.
 -- @tparam function handler Function handler called with arguments passed when `:Fire(...)` is called
 -- @treturn Connection Connection object that can be disconnected
 function Signal:Connect(handler)
-	if not self._bindableEvent then return error("Signal has been destroyed"); end --Fixes an error while respawning with the UI injected
+    if not self._bindableEvent then
+        warn("Attempting to connect to a destroyed signal")
+        return nil
+    end
 
-	if not (type(handler) == "function") then
-		error(("connect(%s)"):format(typeof(handler)), 2)
-	end
+    if type(handler) ~= "function" then
+        error("Connect argument must be a function", 2)
+    end
 
-	return self._bindableEvent.Event:Connect(function()
-		handler(unpack(self._argData, 1, self._argCount))
-	end)
+    return self._bindableEvent.Event:Connect(function()
+        handler(unpack(self._argData, 1, self._argCount))
+    end)
 end
 
 --- Wait for fire to be called, and return the arguments it was given.
 -- @treturn ... Variable arguments from connection
 function Signal:Wait()
-	self._bindableEvent.Event:Wait()
-	assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
-	return unpack(self._argData, 1, self._argCount)
+    self._bindableEvent.Event:Wait()
+    assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
+    return unpack(self._argData, 1, self._argCount)
 end
 
 --- Disconnects all connected events to the signal. Voids the signal as unusable.
 -- @treturn nil
 function Signal:Destroy()
-	if self._bindableEvent then
-		self._bindableEvent:Destroy()
-		self._bindableEvent = nil
-	end
+    if not self._bindableEvent then
+        warn("Signal has already been destroyed")
+        return
+    end
 
-	self._argData = nil
-	self._argCount = nil
+    self._bindableEvent:Destroy()
+    self._bindableEvent = nil
+    self._argData = nil
+    self._argCount = nil
 end
 
 return Signal
