f278623462cc83f18d6193dd0cc2cf531d0e7c8a
blackout <74207478+Blackout4781@users.noreply.github.com>
2024-02-25 00:16:41 -0600

    Update createBaseESP.lua

--- utils/createBaseESP.lua
+++ utils/createBaseESP.lua
@@ -1,8 +1,8 @@
-local Maid = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/utils/Maid.lua'))()
-local Services = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/utils/Services.lua'))()
+local Maid = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/main/utils/Maid.lua'))()
+local Services = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/main/utils/Services.lua'))()
 
-local toCamelCase = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/utils/toCamelCase.lua'))()
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/UILibrary.lua'))()
+local toCamelCase = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/main/utils/toCamelCase.lua'))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackout4781/Synatii-Services/main/UILibrary.lua'))()
 
 local Players, CorePackages, HttpService = Services:Get('Players', 'CorePackages', 'HttpService');
 local LocalPlayer = Players.LocalPlayer;
